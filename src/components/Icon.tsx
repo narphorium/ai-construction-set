5c0380f2aeee84ae4cd13768bf98f441cf99c456
Shawn Simister <narphorium@gmail.com>
2023-12-03 22:40:37 -0800

    Remove Icon

--- src/components/Icon.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export interface IconProps {
-  svg: string
-}
-
-export const Icon = function Icon ({ svg }: IconProps): JSX.Element {
-  return <span dangerouslySetInnerHTML={{ __html: svg }}></span>
-}
